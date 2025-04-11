ea359e76ed7cca9631afa607db7ff32464a24191
abraao.reis <abraao.reis2012@gmail.com>
2025-04-11 13:54:45 -0300

    remove error component

--- src/component/table/table.tsx
+++ src/component/table/table.tsx
@@ -1,39 +1,39 @@
 import { MigrationData } from "@/model/form.type";
-import { Alert, Button, Skeleton, Table } from "antd";
+import { Skeleton, Table } from "antd";
 import { useTable } from "./useTable";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 
 export const TableMigration = () => {
-  const { columns, migrationData, error, loading } = useTable();
-  const navigation = useRouter();
-  if (error) {
-    return (
-      <div className={`p-4`}>
-        <Alert
-          message="Error"
-          description={
-            <div className="space-y-2">
-              Houve um erro ao consultar informação na base Voltbrás
-            </div>
-          }
-          type="error"
-          showIcon
-          className="mb-4"
-        />
-        <Button
-          type="primary"
-          variant="link"
-          onClick={() => {
-            navigation.push("/");
-          }}
-          className="bg-blue-600 hover:bg-blue-700 py-4"
-        >
-          Retry
-        </Button>
-      </div>
-    );
-  }
+  const { columns, migrationData, loading } = useTable();
+  // const navigation = useRouter();
+  // if (error) {
+  //   return (
+  //     <div className={`p-4`}>
+  //       <Alert
+  //         message="Error"
+  //         description={
+  //           <div className="space-y-2">
+  //             Houve um erro ao consultar informação na base Voltbrás
+  //           </div>
+  //         }
+  //         type="error"
+  //         showIcon
+  //         className="mb-4"
+  //       />
+  //       <Button
+  //         type="primary"
+  //         variant="link"
+  //         onClick={() => {
+  //           navigation.push("/");
+  //         }}
+  //         className="bg-blue-600 hover:bg-blue-700 py-4"
+  //       >
+  //         Retry
+  //       </Button>
+  //     </div>
+  //   );
+  // }
 
   if (loading) {
     return <Skeleton active paragraph={{ rows: 6 }} />;
